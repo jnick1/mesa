7a6570fe0c4e20f4425baa7dd61c103385e0850a
FlamingFoxNinja <sierraaubrey@sbcglobal.net>
2016-02-11 20:23:36 -0500

    test
    test

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
         // put your code here
-        ?>
+        //hi, can you see this?
+                ?>
     </body>
 </html>
