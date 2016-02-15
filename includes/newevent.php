d3f744f444edd8e23fe42f9148b3499f7cd0da15
Jacob <janick@oakland.edu>
2016-02-15 03:17:19 -0500

    New event and main CSS page added
    Added files for the aforementioned files, no real content yet.

---
+++ includes/newevent.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+$homedir = "../";
+?>
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="<?php echo $homedir."css/main.css"; ?>">
+        <title>Meeting and Event Scheduling Assistant: New Event</title>
+    </head>
+    <body>
+        
+    </body>
+</html>
