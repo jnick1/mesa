2adc0748f9eb7d57f2d96d3644291393614d2e46
Will Thompson <wpthompson@oakland.edu>
2016-02-18 20:55:54 -0500

    Added json output to page of events in primary calendar of user

--- google-service-access.php
+++ google-service-access.php
@@ -10,9 +10,10 @@ $client->addScope(Google_Service_Calendar::CALENDAR_READONLY);
 
 if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
   $client->setAccessToken($_SESSION['access_token']);
-  $drive_service = new Google_Service_Calendar($client);
-  $files_list = $drive_service->files->listFiles(array())->getItems();
-  echo json_encode($files_list);
+  $event_service = new Google_Service_Calendar($client);
+  $optParams = array('orderBy' => "startTime");
+  $events_list = $event_service->events->listEvents('primary');
+  echo json_encode($events_list);
 } else {
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
