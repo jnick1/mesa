064461efd17aca2630f27f3686b393f644128cbf
William Thompson <wpthompson@oakland.edu>
2016-02-20 02:56:53 -0500

    Added comments to illustrate process of OAuth more clearly

--- google-service-access.php
+++ google-service-access.php
@@ -8,15 +8,15 @@ $client = new Google_Client();
 $client->setAuthConfigFile(CLIENT_SECRET_PATH);
 $client->addScope(Google_Service_Calendar::CALENDAR_READONLY);
 
-if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+if (isset($_SESSION['access_token']) && $_SESSION['access_token']) { //Check if have access token
   $client->setAccessToken($_SESSION['access_token']);
   $event_service = new Google_Service_Calendar($client);
   $optParams = array('orderBy' => "startTime");
-  $events_list = $event_service->events->listEvents('primary');
-  echo json_encode($events_list);
+  $events_list = $event_service->events->listEvents('primary'); //Gets all events in player's calendar
+  echo json_encode($events_list); //Output Json results, temporary
 } else {
   $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/oauth2callback.php';
-  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL)); //Redirect to ask user authentication
 }
 ?>
 
--- oauth2callback.php
+++ oauth2callback.php
@@ -1,6 +1,7 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
+
 session_start();
 
 $client = new Google_Client();
@@ -12,9 +13,9 @@ if (! isset($_GET['code'])) {
   $auth_url = $client->createAuthUrl();
   header('Location: ' . filter_var($auth_url, FILTER_SANITIZE_URL));
 } else {
-  $client->authenticate($_GET['code']);
-  $_SESSION['access_token'] = $client->getAccessToken();
-  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/google-service-access.php';
+  $client->authenticate($_GET['code']); //Authenticate client with code
+  $_SESSION['access_token'] = $client->getAccessToken(); //Store access token in session
+  $redirect_uri = 'http://' . $_SERVER['HTTP_HOST'] . '/google-service-access.php'; //Switch back to data retrieval
   header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
 }
 ?>
