891079a1157f94a0645a1e44dce023f4a99019fd
FlamingFoxNinja <sierraaubrey@sbcglobal.net>
2016-03-07 17:35:36 -0500

    working on the basic
    working on the code on the basic stuff, like searching through the
    matricies and stuff. There are some parts I'm not sure how to get the
    info from the Google integration stuff, so I left it as manual input
    currently

--- python/OptCode.py
+++ python/OptCode.py
@@ -8,32 +8,72 @@
 if __name__ == "__main__":
     print "Hello World"
 
- end = 5 #object
- start = 6 #object
- attendies = {2, 3, 4, 4} #list of people 
+ end = input('When to end') #what is the time set to stop the search
+ start = input('When to start') #where to start the search
+  searchWidth = input('How Many Days are available')
+ gran = input('How many minutes do want to increment the search by?')  #hour, can also be wither 15mins or 1 mins depending on user input
+ grularity = gran/60 #search it by the hour
+ bannedtimes = input('What time increments are not allowed') #banned granularity increments based on user input
  location = "place"
  reoccurance = {} #list of the number of times it happens, and holds multiple meetings
  
- searchWidth = 7 #user based input
- granularity = 1 #hour, can also be wither 15mins or 1 mins depending on user input
- bannedtimes = 8 #banned granularity increments based on user input
+ #matrices necessary
+ masterCalender = [ [ 0 for i in range(7) ] for j in range(24) ] #calender for the final date selection, full week 7 days/24hrs
+ normCalender = [ [ 0 for i in range(searchWidth) ] for j in range((24/granularity) - bannedtimes) ] #specific calender for that person
  
- #matrices necessary 
- masterCalender = (searchWidth, (24/granularity) - bannedtimes)
- normCalender = (searchWidth, (24/granularity) - bannedtimes)
- 
- for(i = 0; i<searchWidth; i++)
+  
+ attendies[] = [] #list of people on the project, those who accept
+ attend = 0 # number of people who accepts
+ if (   ): #person accepts
  {
-    for(j=0; j<(24/granularity) - bannedtimes; j+granularity)
+    #moving their Goolge calender to a normCalender
+    for(i = 0; i<searchWidth; i++):
     {
-        #if there is an evern
-            NormCalender(i,j) = 1
-        #else
-            
+        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
+        {
+            if GoogleCalender[i][j] = 'event': 
+                NormCalender[i][j] = 1
+            else:
+                NormCalender[1][j] = 0          
+        }
     }
- }
     
+    #adding the normCal to the Master calender
+    for(i = 0; i<searchWidth; i++):
+    {
+        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
+        {
+            if NormCalender[i][j] = 1: 
+                Blank = masterCalender[i][j]
+                Blank = Blank + '1'
+                masterCalender[i][j] = Blank
+            else:
+                Blank = masterCalender[i][j]
+                Blank = Blank + '0'
+                masterCalender[i][j] = Blank        
+        }
+    }
+    
+    
+    
+ }
+  
+ #Figuring out which has the most attendies
+  for(i = 0; i<7; i++):
+    {
+        for(j=0; j<24; j++): #should it be incremented by granularity or 1, I'm not sure
+        {
+             info = masterCalender[i][j]
+             for (i =0; i<length.info; i++):
+                 if (0 in info):
+                     attend +=1
+                 
+                    
+        }
+    }
  
  
  
-
+ #hheh
+ 
+ 
