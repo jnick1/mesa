0fca605791c04c76ce92c49710a812282a3603dd
FlamingFoxNinja <sierraaubrey@sbcglobal.net>
2016-03-10 19:47:15 -0500

    updating work

--- python/OptCode.py
+++ python/OptCode.py
@@ -8,16 +8,20 @@
 if __name__ == "__main__":
     print "Hello World"
 
+#using numpty for matrix
+import numpty as np
+
  #necessary objects needed before starting the program
  
- Priority[ ] = [1, 2, 3, 4, 5, 6, 7] #each one is a different function 
+ Priority[] = [1, 2, 3, 4, 5, 6, 7] #each one is a different function 
  reoccurance = {} #list of the number of times it happens, and holds multiple meetings
  attendies[] = [] #list of people on the project, those who accept
  attend = 0 # number of people who accepts
  
  #matrices necessary
  masterCalender = [ [ 0 for i in range(7) ] for j in range(24) ] #calender for the final date selection, full week 7 days/24hrs
- 
+ googleCalender[]
+ googleCal = [ [ 0 for i in range(7) ] for j in range(24) ] #calender to import 
 
  
  if (   ): #person accepts
@@ -35,24 +39,38 @@ if __name__ == "__main__":
     
     #having the priority set list seperate because idk if there will be exact results if its at the end of the other file
     g = open('PriorityTextFile', 'r') 
-    for (i=0; i<7; i++)
+    for (i=0; i<7; i++):
         test = Priority[i]
         Priority[i] = test * g.readline() #each will get either LOW(1), MED(10), or HIGH(100) to determine the importance value
     g.close() #closing priority file
- 
+    
+    #input the google calender Trial 1
+    with open('OTP.txt','r') as h:
+            for line in h:
+                for word in line.split(): #delimiter is a space, word is now a list
+                    print(word)  
+    
+    
+     #input the google calender trial 2
+    with open('GoogleCalenderTest.txt','r') as h:
+    for line in h:
+        for word in line.split():
+                    googleCalender[].append(word)
+    
+    
+     
     #temp specific calender for that person
     normCalender = [ [ 0 for i in range(searchWidth) ] for j in range((24/granularity) - bannedtimes) ] 
     
-    #need to learn to input the google calender, ask that tommorow meeting
     #moving their Goolge calender to a normCalender
     for(i = 0; i<searchWidth; i++):
     {
         for(j=0; j<(24/granularity) - bannedtimes; j+granularity): #hard to tell what to increment j by because it varies with each person
         {
             if GoogleCalender[i][j] = 'event':  #if there is an event in the google calender
-                NormCalender[i][j] = 1
+                NormCalender[i][j] = '1'
             else:
-                NormCalender[1][j] = 0          
+                NormCalender[1][j] = '0'          
         }
     }
     
@@ -63,28 +81,33 @@ if __name__ == "__main__":
     
     
     #adding the normCal to the Master calender
-    for(i = 0; i<searchWidth; i++):
-    {
-        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
-        {
-            #
-            if NormCalender[i][j] = 1: 
-                Blank = masterCalender[i][j]
-                Blank = Blank + '1'
-                masterCalender[i][j] = Blank
-            else:
-                Blank = masterCalender[i][j]
-                Blank = Blank + '0'
-                masterCalender[i][j] = Blank        
-        }
-    }
-    
- }
+    AdditionMatrix(NormCalender, masterCalender):
+ }   
+ 
+ #editing the Master Calender depending on the priority system
+ for (i=7; i>0; i--):
+    if (Priority[i] = 7):
+         #
+    else if (Priority[i] = 6):
+         #
+    else if (Priority[i] = 5):
+         #
+    else if (Priority[i] = 4):
+         #
+    else if (Priority[i] = 3):
+         #
+    else if (Priority[i] = 2):
+         #
+    else if (Priority[i] = 1): #
+         
+  
+ 
+ 
  
  
  
  #adding the matrixes together
- def AdditionMatrix(A[][], masterCalender[][]):
+ def AdditionMatrix(Array A[][], Array masterCalender[][]):
 {  
     For (int i =startdays; i <searchwidth; i++):
     {
@@ -95,6 +118,8 @@ if __name__ == "__main__":
         }
     }
 }
+
+
 
     List []
     for(i = 0; i<7; i++):
@@ -111,55 +136,39 @@ if __name__ == "__main__":
     
     #sorting the list so that the biggest number is on top
     List.sort(reverse = True)
-        
-    #top three spots (code will change depending on the reoccurance)
-    First = List[0]
-    Second = List[1]
-    Third = List[2]
+      
     
-    #finding the time based on the most attendees reported
-    def findMostRequested(A[][]): 
-        for(i = 0; i<searchWidth; i++):
-    {
-        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
-        {
-            if (A[i][j] == First)
-                A[i][j] = -1
-                return 'Top Priority is [' + i + '], [' + j + ']'
-        }
+    #picking the top numbered spots depending on the recursion
+    def multiSpots(Array List1[], Array matrix[][], int weekly):
+    {temp = 0;
+    do:
+        {for(i = 0; i<searchWidth; i++):
+            {for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
+                #checks, in order, if the number is in the top 5 spots, adds to list if so
+                {if (matrix[i][j] == List1[0]):
+                    matrix[i][j] = -1
+                    reoccurance.insert("Top Priotity is " + i+ " at " + j + " time")
+                    temp +=1
+                else if (matrix[i][j] == List1[1]):
+                    matrix[i][j] = -1
+                    reoccurance.insert("Priotity is " + i+ " at " + j + " time")
+                    temp +=1
+                else if (matrix[i][j] == List1[2]):
+                    matrix[i][j] = -1
+                    reoccurance.insert("Priotity is " + i+ " at " + j + " time")
+                    temp +=1
+                else if (matrix[i][j] == List1[3]):
+                    matrix[i][j] = -1
+                    reoccurance.insert("Priotity is " + i+ " at " + j + " time")
+                    temp +=1
+                else if (matrix[i][j] == List1[4]):
+                    matrix[i][j] = -1
+                    reoccurance.insert("Priotity is " + i+ " at " + j + " time")
+                    temp +=1
+                } 
+            }
+        } while (weekly > temp)
     }
     
-    #finding the time based on the most attendees reported
-    def find2ndMostRequested(A[][]): 
-        for(i = 0; i<searchWidth; i++):
-    {
-        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
-        {
-            if (A[i][j] == Second && A[i][j] != -1 )
-                A[i][j] = -1
-                return 'Second Priority is [' + i + '], [' + j + ']'
-        }
-    }
-    #finding the time based on the most attendees reported
-    def find3rdMostRequested(A[][]): 
-        for(i = 0; i<searchWidth; i++):
-    {
-        for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
-        {
-            if (A[i][j] == Third && A[i][j] != -1 )
-                A[i][j] = -1
-                return 'Second Priority is [' + i + '], [' + j + ']' 
-               #returning the spot as text for now until I know what to return it better
-        }
-    }
+    #IDEA: create a blank matrix to hold ONLY the picked numbers times to return
     
-    
-    
-    
-    
-    
-    
-
-
-    
- 
