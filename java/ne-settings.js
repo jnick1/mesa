3cedae467fa5cdbac99b0e61dc4a14f89dbbeb9c
Jacob <janick@oakland.edu>
2016-03-19 05:24:18 -0400

    Testing out a new library, and made a new branc
    I'm testing out a new library for some optional features we may (though
    unlikely) implement, and since this work is still an experimental WIP, I
    created a new branch to keep it separate from the rest of the interface.
    
    For the time being, this is only temporary, and will be deleted after
    I've either got it into a working state, or decided that it's not worth
    the time and effort.

--- java/ne-settings.js
+++ java/ne-settings.js
@@ -7,7 +7,7 @@
 var settingsset = false;
 
 $(function(){
-    $("#ne-evt-settings-maxday").datepicker({ dateformat: "mm/dd/yy"});
+    $("#ne-evt-settings-maxdate").datepicker({ dateformat: "mm/dd/yy"});
 });
 
 jQuery.fn.center = function(parent) {
@@ -27,6 +27,7 @@ return this;
 function show_settings_dialogbox(){
     $("#wpg").addClass("ui-popup-background-effect");
     $("#ne-settings-wrapper").addClass("ui-popup-active");
+    $("#ne-settings-dialogbox").center();
 }
 function hide_settings_dialogbox(){
     $("#wpg").removeClass("ui-popup-background-effect");
@@ -49,9 +50,18 @@ function reset_settings(){
     $("#ne-evt-settings-attendancegate,#ne-evt-settings-blacklistgate,#ne-evt-settings-daygate,"+
         "#ne-evt-settings-durationgate,#ne-evt-settings-locationgate,#ne-evt-settings-repeatgate,"+
         "#ne-evt-settings-timegate").prop("checked", false).prop("disabled", true);
-$("#ne-evt-settings-attendeesallow,#ne-evt-settings-blacklistallow,#ne-evt-settings-dayallow,"+
-        "#ne-evt-settings-durationallow,#ne-evt-settings-locationallow,#ne-evt-settings-repeatsallow,"+
-        "#ne-evt-settings-timeallow").prop("checked", true);
+    $("#ne-evt-settings-attendeesallow,#ne-evt-settings-dayallow,#ne-evt-settings-durationallow,"+
+        "#ne-evt-settings-locationallow,#ne-evt-settings-repeatsallow,#ne-evt-settings-timeallow").prop("checked", true);
+    $("#ne-evt-settings-blacklistdays-0,#ne-evt-settings-blacklistdays-1,#ne-evt-settings-blacklistdays-2,"+
+            "#ne-evt-settings-blacklistdays-3,#ne-evt-settings-blacklistdays-4,#ne-evt-settings-blacklistdays-5,"+
+            "#ne-evt-settings-blacklistdays-6").prop("checked", false);
+    $("#ne-settings-attendees-table,#ne-settings-blacklist-table,#ne-settings-day-table,"+
+            "#ne-settings-duration-table,#ne-settings-location-table,#ne-settings-repeats-table,"+
+            "#ne-settings-time-table").addClass("wpg-nodisplay");
+    $("#ne-evt-settings-repeatsmin").val("1");
+    $("#ne-evt-settings-minduration").val("00:30");
+    $("#ne-evt-settings-blackliststart").val("12:00am");
+    $("#ne-evt-settings-blacklistend").val("11:59pm");
 }
 
 $(document).ready(function(){
@@ -71,7 +81,7 @@ $(document).on("click", "#ne-evt-settingsbox", function(){
         $("#ne-settings-display").addClass("wpg-nodisplay");
         $("#ne-label-settingsbox").html("Advanced settings");
     }
-    $("#ne-settings-dialogbox").center();
+    
 });
 
 $(document).on("click", "#ne-settings-x, #ne-settings-btn-cancel", function(){
@@ -116,4 +126,65 @@ $(document).on("change", "#ne-evt-settings-attendancegate,#ne-evt-settings-black
 
 $(document).on("click", "#ne-settings-edit", function(){
     show_settings_dialogbox();
+});
+
+$(document).on("click", "#ne-btn-test", function(){
+    
+    var guest = $("#ne-container-guests");
+    
+    var guestpos = guest.offset();
+    var guestwidth = guest.width();
+    var guestheight = guest.height();
+    
+    $("#test-top").width("100%").height(guestpos.top+"px").position({top: 0, left: 0});
+    $("#test-bottom").width("100%").height("calc(100% - "+(guestpos.top+guestheight)+"px)").position({top: (guestpos.top+guestheight), left: 0});
+    $("#test-left").width(guestpos.left+"px").height("100%").position({top: 0, left: 0});
+    $("#test-right").width().height("100%").position({top: 0, left: (guestpos.left+guestwidth)});
+    
+    $("#test-top,#test-bottom,#test-left,#test-right").css("background-color", "#f00");
+});
+
+$(document).ready(function () {
+    frost = function () {
+        var w = $('#main-view').width();
+        html2canvas(document.body, {
+            onrendered: function (canvas) {
+                document.body.appendChild(canvas);
+                $('canvas').wrap('<div id="contain" />');
+            },
+            width: w,
+            height: 30
+        });
+        $('canvas, #partial-overlay, #cover').hide();
+        $('#cover').fadeIn('slow', function () {
+            $('#partial-overlay').fadeIn('slow');
+        });
+    };
+
+    $('body').append('<div id="cover"></div><svg id="svg-image-blur"><filter id="blur-effect-1"><feGaussianBlur stdDeviation="2"/></filter></svg>');
+
+    $('#main-view').click(function () {
+        frost();
+        $('#partial-overlay').addClass('vis');
+        $(window).resize(function () {
+            $('canvas, #partial-overlay, #cover').hide();
+        });
+
+        function onResize() {
+            if ($('#partial-overlay').hasClass('vis')) {
+                frost();
+            }
+        }
+        var timer;
+        $(window).bind('resize', function () {
+            timer && clearTimeout(timer);
+            timer = setTimeout(onResize, 50);
+        });
+
+    });
+
+    $('#partial-overlay').click(function () {
+        $('#partial-overlay').removeClass('vis');
+        $('canvas, #partial-overlay, #cover').hide();
+    });
 });
