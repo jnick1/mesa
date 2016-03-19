fa52a5f0be93b721e58652d8bd134856e54acca4
FlamingFoxNinja <sierraaubrey@sbcglobal.net>
2016-03-19 13:30:06 -0400

    hourly work update 1

--- python/OptCode.py
+++ python/OptCode.py
@@ -9,6 +9,11 @@ import numpty as np
 if __name__ == "__main__":
     print "Hello World"
 
+#IMPORTANT!!!
+#RE-WRITE IT SO THAT THE PRIORITY IS MANIPULATED ONLY
+BY 
+
+
 #using numpty for matrix
 
 #necessary objects needed before starting the program
@@ -23,10 +28,7 @@ if __name__ == "__main__":
  googleCalender[]
  googleCal = [ [ 0 for i in range(7) ] for j in range(24) ] #calender to import 
  
- 
- if (   ): #person accepts
- {
-    #first, enter the necessary data
+ #first, enter the necessary data
     f = open('WillsBasicInfo', 'r') #the order of the text file should be as the following: 
     endTime = f.readline() #what is the time set to stop the search
     startTime = f.readline() #where to start the search
@@ -39,18 +41,20 @@ if __name__ == "__main__":
     f.close()#closing the file
     grularity = gran/60 #search it by what people wanted, else it's automatically 1 hour
     
+    #MAY OR MAY NOT USE DEPENDING
     #having the priority set list seperate because idk if there will be exact results if its at the end of the other file
-    g = open('PriorityTextFile', 'r') 
-    for (i=0; i<7; i++):
-        test = Priority[i]
-        Priority[i] = test * g.readline() #each will get either LOW(1), MED(10), or HIGH(100) to determine the importance value
-    g.close() #closing priority file
-    
+    #g = open('PriorityTextFile', 'r') 
+    #for (i=0; i<7; i++):
+    #    test = Priority[i]
+    #    Priority[i] = test * g.readline() #each will get either LOW(1), MED(10), or HIGH(100) to determine the importance value
+    #g.close() #closing priority file
     #same as priority list
-    k = open('specificDaysTextFile', 'r')
-        DAYS = k.read().split(',') #insert number for specific day, 0=SUN, 1=MON, etc
-    k.close() #closing day list
-    
+    #k = open('specificDaysTextFile', 'r')
+    #    DAYS = k.read().split(',') #insert number for specific day, 0=SUN, 1=MON, etc
+    #k.close() #closing day list
+ 
+ if (   ): #person accepts
+ {
     #input the google calender 
     with open('GoogleCalenderTest.txt','r') as h:
     for line in h:
@@ -58,34 +62,31 @@ if __name__ == "__main__":
                     googleCalender[].append(word)
     h.close() 
     
-    #temp specific calender for that person
-    #normCalender = [ [ 0 for i in range(searchWidth) ] for j in range((24/granularity) - bannedtimes) ] 
-    normCalender = [ [ 0 for i in range(7) ] for j in range(24) ]
-    
     #moving their Goolge calender to a normCalender
     for(i = 0; i<searchWidth; i++):
     {
         for(j=0; j<(24/granularity) - bannedtimes; j+granularity): #hard to tell what to increment j by because it varies with each person
         {
             if GoogleCalender[i][j] = '1':  #if there is an event in the google calender
-                NormCalender[i][j] = '1' 
+                peopleCalender[i][j] += '1' 
             else:
-                NormCalender[1][j] = '0'          
+                peopleCalender[1][j] += '0'          
         }
     }
-     attendies.insert(NormCalender)
-    #saving the normal calender into a list to hold on too
-    #This is a guess, I really don't think that this would work
-    #will do further research on it
-
+     attendies.insert(peopleCalender)
+    #saving the calender into a list to hold on too (hopefully)
     
-    for (i=7; i>0; i--):
+ }#END ATTENDIES IF STATEMENT
+    
+
+    #adjust this based on time and such
+for (i=7; i>0; i--):
         if (i = 7):#number of people attending
         {   attend = 0 # number of people who accepts
             for (i =0; i <searchwidth; i++):
                 {for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
                     {
-                        Attendees = peopleCalender[i][j] + NormCalender[i][j] #adding the matrixes together
+                        Attendees = peopleCalender[i][j]
                         peopleCalender[i][j] = Attendees #storing it back for the next list
                     
                         for (i =0; i<length.info; i++): #don't know if its necessary or not, but will leave it in at the moment
@@ -105,42 +106,11 @@ if __name__ == "__main__":
     else if (i = 3): #Duration of meeting
          #
     else if (i = 2): #Day of week
-         {for (i=0; i<DAYS.length(); i++)
-            {for (j=0; j<(24/granularity) - bannedtimes; j+granularity): #do this for each hour 
-               {day = DAYS[i] #reads number for the specific day to correlate with the matrix
-                if(NormCalender[day][j] == 0): #search in day range
-                {
-                    points = testCal[day][j]
-                    masterCalender[day][j] = points + Priority[2]
-                }
-               } 
-            }
+        {DayLocation ( DAYS, granlarity, bannedtimes, masterCalender, NormCalender, Priority)
         }
     else if (i = 1): #Time of day
-        {for (i = 0; i<DAYS.length(); i++): #do this for each day 
-            {day = DAYS[i]
-            if(NormCalender[day][preferedTime] == 0): #original perfered Time of day
-                {
-                    points = masterCalender[day][preferedTime]
-                    masterCalender[day][preferedTime] = points + Priority[1]
-                }
-            else:
-                spot = False
-                count = 1
-                do:
-                    { preferedTime = preferedTime + (count* (-1^count))#branching out hour by hour
-                    if(NormCalender[day][preferedTime] == 0): #modified perfered Time of day
-                        {
-                            points = testCal[i][preferedTime]
-                            masterCalender[day][preferedTime] = points + Priority[1]
-                            spot = True #get out of loop early
-                        }
-            
-                    }while ( spot == False && preferedTime >=  || spot == False && preferedTime <= end ) 
-                    #this way, if the perfered time is not in the middle, it will still performed the action
-            }
-        }
-    
+    {    TimeLocation (DAYS, NormCalender, MasterCalender, Priority, preferedTime)
+    }
     
     List [] #used to hold the numbers in the matrix
     for(i = 0; i<7; i++):
@@ -156,16 +126,21 @@ if __name__ == "__main__":
     
     returnCalender[][]
     #pick the top spots and return them to a matrix
-    {for(k=0; k<weekly; k++)
+    k = 0
+    l = 0
+    do{
         {for(i = 0; i<searchWidth; i++):
             {for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
                 #checks for the top spots
-                {if (masterCalender[i][j] == List1[k]):
+                {if (masterCalender[i][j] == List1[l]):
                     returnCalender[i][j] = masterCalender[i][j]
+                    k++
                 } 
             }
         }
-    }
+        l = l+1
+    } while(k<(weekly*2))
+    
     
     #writing it to a text file
     for row in returnCalender:
@@ -180,6 +155,70 @@ if __name__ == "__main__":
     
     
  }#end of attendee acceptance
+
+def DayLocation ( DAYS, granlarity, bannedtimes, masterCalender, NormCalender, Priority)
+{for (i=0; i<DAYS.length(); i++)
+            {for (j=0; j<(24/granularity) - bannedtimes; j+granularity): #do this for each hour 
+               {day = DAYS[i] #reads number for the specific day to correlate with the matrix
+                if(NormCalender[day][j] == 0): #search in day range
+                {
+                    points = masterCalender[day][j]
+                    masterCalender[day][j] = points + Priority[2]
+                }
+               } 
+            }
+}
     
-   
-    
+def TimeLocation (DAYS, NormCalender, MasterCalender, Priority, preferedTime, granularity)
+{for (i = 0; i<DAYS.length(); i++): #do this for each day 
+            {day = DAYS[i]
+            if(NormCalender[day][preferedTime] == 0): #original perfered Time of day
+                {
+                    points = masterCalender[day][preferedTime]
+                    masterCalender[day][preferedTime] = points + Priority[1]
+                }
+            else:
+                spot = False
+                count = 1
+                do:
+                    { preferedTime = preferedTime + (granularity*count* (-1^count))#branching out hour by hour
+                    if(NormCalender[day][preferedTime] == 0): #modified perfered Time of day
+                        {
+                            points = masterCalender[i][preferedTime]
+                            masterCalender[day][preferedTime] = points + Priority[1]
+                            spot = True #get out of loop early
+                        }
+                    else:
+                        count++            
+                    }while ( spot == False && preferedTime >=  || spot == False && preferedTime <= end ) 
+                    #this way, if the perfered time is not in the middle, it will still performed the action
+            }
+        }
+
+def peopleCounting(searchwidth, granularity, bannedtimes, peopleCalender):
+    {    attend = 0 # number of people who accepts
+            for (i =0; i <searchwidth; i++):
+                {for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
+                    {
+                        Attendees = peopleCalender[i][j]
+                                           
+                        for (i =0; i<length.info; i++): #don't know if its necessary or not, but will leave it in at the moment
+                            {if (0 in Attendees):
+                                attend +=1
+                            }
+                        
+                        masterCalender[i][j] = attend
+                    }
+                }
+    }
+
+#For {for(i = 0; i<searchWidth; i++)
+#   {for(j=0; j<(24/granularity) - bannedtimes; j+granularity):
+#       for(location
+#          for(recursion
+#               for(day
+#                   for(time
+#                       for(people
+#meeting duration time practice
+
+
