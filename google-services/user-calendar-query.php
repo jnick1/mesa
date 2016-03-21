51d6b81f115eb838ce8f1b4c4e94038c20f30986
William Thompson <wpthompson@oakland.edu>
2016-03-21 01:26:14 -0400

    Separation into functions. Basis for specifying calendars to use.

--- google-services/user-calendar-query.php
+++ google-services/user-calendar-query.php
@@ -1,28 +1,24 @@
 <?php
 require __DIR__ . '/google-services-header.php';
-
-if (isset($_SESSION['calendar_summaries']) && $_SESSION['calendar_summaries']):
-    ?>
-    <form method='post' action='#'> 
-        <table> <tr>
-                <td>Select Calendars To Use</td>
-                <td>
-                    <?php
-                    $calendar_list = $_SESSION['calendar_summaries'];
-                    foreach ($calendar_list as $calendar):
-                        ?>
-                        <input type='checkbox' name='checkboxvar[]' value='<?php $calendar ?>' ><?php echo $calendar ?><br>
-                        <?php
-                    endforeach;
-                    ?>
-                </td> </tr> 
-        </table> 
-        <input type='submit'> 
-    </form>
-<?php
-else:
-    echo 'No calendar list! ERROR';
-endif;
 ?>
-
-
+<form method='post' action='#'> 
+    <table> <tr>
+            <td>Select Calendars To Use</td>
+            <td>
+                <?php
+                $calendar_list = $_SESSION['calendar_summaries'];
+                foreach ($calendar_list as $calendar):
+                    ?>
+                    <input type='checkbox' name='checkboxvar[]' value='<?php echo $calendar ?>' ><?php echo $calendar ?><br>
+                    <?php
+                endforeach;
+                ?>
+            </td> </tr> 
+    </table> 
+    <input type="submit" name="formSubmit" value="Submit" />
+</form>
+<?php
+if(!empty($_POST['checkboxvar'])) {
+    $_SESSION['user_calendar_summaries'] = $_POST['checkboxvar'];
+    redirect_local('google-service-access.php');
+}
