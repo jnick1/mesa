dde1803578fa54474cf622f7bee73625593de62c
Jacob <janick@oakland.edu>
2016-04-02 01:57:20 -0400

    Bug fixes and user registration for attendee fix
    Fixed time being displayed incorrectly on eventlist.php (was in UTC, now
    in local). Also, modified some styling on eventlist.php to allow better
    horizontal scaling. Finally, I've implemented checks to allow for users
    to register for an account with an email that is already taken if there
    is a token that allows them to do this (this is for registering users
    that were attendees in previous events).

--- includes/protocols/register.php
+++ includes/protocols/register.php
@@ -3,19 +3,56 @@ if(isset($scrubbed["register"])) {
     if(isset($_SESSION["pkUserid"]) && is_numeric($_SESSION["pkUserid"])) {
         $warnings[] = "You cannot register a new user account while logged in.";
     } else {
-        $q1 = "SELECT pkUserid FROM tblusers WHERE txEmail = ?";
+        $q1 = "SELECT pkUserid, txHash FROM tblusers WHERE txEmail = ?";
         $q2 = "INSERT INTO `tblusers`(`txEmail`, `blSalt`, `txHash`, `dtLogin`) VALUES (?,?,?,?)";
+        $q3 = "SELECT txEmail, dtExpires FROM tbltokens WHERE txTokenid = ?";
+        $q4 = "SELECT pkUserid FROM tblusers WHERE txEmail = ?";
 
         if($stmt = $dbc->prepare($q1)){
             $stmt->bind_param("s", $scrubbed["in-evt-register-email"]);
             $stmt->execute();
-            $stmt->bind_result($userExists);
+            $stmt->bind_result($userExists,$txHash);
             $stmt->fetch();
             $stmt->free_result();
             $stmt->close();
         }
         if($userExists) {
-            $errors[] = "This email has already been registered. Please register using a different email address.";
+            if($stmt = $dbc->prepare($q3)){
+                $stmt->bind_param("s", $txHash);
+                $stmt->execute();
+                $stmt->bind_result($tokenEmail, $tokenExpires);
+                $stmt->fetch();
+                $stmt->free_result();
+                $stmt->close();
+            }
+            if($tokenEmail && ($tokenExpires === NULL || (time() - strtotime($tokenExpires))<0) && $tokenEmail==$scrubbed["in-evt-register-email"]) {
+                $ivsize = mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CFB);
+                $salt = mcrypt_create_iv($ivsize, MCRYPT_DEV_URANDOM);
+                $hash = hash("sha256",$salt.$scrubbed["in-evt-register-password"]);
+                $login = gmdate("Y-m-d H:i:s");
+
+                if($stmt = $dbc->prepare($q2)){
+                    $stmt->bind_param("ssss", $scrubbed["in-evt-register-email"],$salt,$hash,$login);
+                    $stmt->execute();
+                    $stmt->free_result();
+                    $stmt->close();
+                }
+                if($stmt = $dbc->prepare($q4)){
+                    $stmt->bind_param("s", $scrubbed["in-evt-register-email"]);
+                    $stmt->execute();
+                    $stmt->bind_result($pkUserid);
+                    $stmt->fetch();
+                    $stmt->free_result();
+                    $stmt->close();
+                }
+                $_SESSION["pkUserid"] = $pkUserid;
+                $_SESSION["email"] = $scrubbed["in-evt-register-email"];
+                $_SESSION["lastLogin"] = $login;
+                $_SESSION["userColor"] = "#".substr($hash, 0, 6);
+                $notifications[] = "Success! Your user account has been successfully created. We hope you enjoy your Mesa experience!";
+            } else {
+                $errors[] = "This email has already been registered. Please register using a different email address.";
+            }
         } else {
             $ivsize = mcrypt_get_iv_size(MCRYPT_CAST_256, MCRYPT_MODE_CFB);
             $salt = mcrypt_create_iv($ivsize, MCRYPT_DEV_URANDOM);
@@ -28,7 +65,7 @@ if(isset($scrubbed["register"])) {
                 $stmt->free_result();
                 $stmt->close();
             }
-            if($stmt = $dbc->prepare($q1)){
+            if($stmt = $dbc->prepare($q4)){
                 $stmt->bind_param("s", $scrubbed["in-evt-register-email"]);
                 $stmt->execute();
                 $stmt->bind_result($pkUserid);
--- java/el-time.js
+++ java/el-time.js
@@ -13,13 +13,31 @@ $(document).ready(function() {
     });
     var today = new Date();
     for(var time in parsedtimes) {
-        var newtime = new Date(today.getUTCFullYear()+"/"+(today.getUTCMonth()+1)+"/"+today.getUTCDate()+" "+parsedtimes[time]+":00 UTC ");
-
+        var start_date = $("#"+time).prev(".el-content-event-date");
+        var end_date = $("#"+time).next(".el-content-event-date");
+        var newtime = new Date();
+        
+        if(start_date.length !== 0) {
+            newtime = new Date(start_date.html().trim() + " " + $("#"+time).html().trim() + ":00 UTC");
+        } else if(end_date.length !== 0) {
+            newtime = new Date(end_date.html().trim() + " " + $("#"+time).html().trim() + ":00 UTC");
+        }
+        
+        var year = newtime.getFullYear();
+        var month = newtime.getMonth()+1;
+        var date = newtime.getDate();
         var hours = newtime.getHours();
         var minutes = newtime.getMinutes();
-
+        
         var suffix = "am";
-
+        
+        if(month < 10) {
+            month = "0" + month;
+        }
+        if(date < 10) {
+            date = "0" + date;
+        }
+        
         if (hours >= 12) {
             suffix = "pm";
             hours -= 12;
@@ -31,59 +49,12 @@ $(document).ready(function() {
         if (minutes < 10) {
             minutes = "0" + minutes;
         }
-
+        
         $("#"+time).html(hours + ":" + minutes + suffix); //hours1 + ":" + minutes + suffix
+        if(start_date.length !== 0) {
+            start_date.html(month + "/" + date + "/" + year);
+        } else if(end_date.length !== 0) {
+            end_date.html(month + "/" + date + "/" + year);
+        }
     }
-    
-//    var times = $(".el-content-event-time");
-//    times.each(function(){
-//        var start_date = $(this).prev(".el-content-event-date");
-//        var end_date = $(this).next(".el-content-event-date");
-//        var newtime = new Date();
-//        if(start_date.length !== 0) {
-//            if(/^d{1}/.test($(this).val())){
-//                $(this).val("0"+$(this).val());
-//            }
-//            newtime = new Date(start_date + " " + $(this).val() + ":00 UTC");
-//        } else if(end_date.length !== 0) {
-//            if(/^d{1}/.test($(this).val())){
-//                $(this).val("0"+$(this).val());
-//            }
-//            newtime = new Date(end_date + " " + $(this).val() + ":00 UTC");
-//        }
-//        
-//        var year = newtime.getFullYear();
-//        var month = newtime.getMonth();
-//        var date = newtime.getDate();
-//        var hours = newtime.getHours();
-//        var minutes = newtime.getMinutes();
-//
-//        var suffix = "am";
-//        if(month < 10) {
-//            month = "0" + month;
-//        }
-//        if(date < 10) {
-//            date = "0" + date;
-//        }
-//
-//        if (hours >= 12) {
-//            suffix = "pm";
-//            hours -= 12;
-//        }
-//        if (hours === 0) {
-//            hours = 12;
-//        }
-//
-//        if (minutes < 10) {
-//            minutes = "0" + minutes;
-//        }
-//
-//        $(this).html(hours + ":" + minutes + suffix); //hours1 + ":" + minutes + suffix
-//        if(start_date.length !== 0) {
-//            start_date.html(month + "/" + date + "/" + year);
-//        } else if(end_date.length !== 0) {
-//            end_date.html(month + "/" + date + "/" + year);
-//        }
-//    });
-    
 });
