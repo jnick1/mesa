25b178b827ce0903100c1d5beec5b3232d905a20
Jacob <janick@oakland.edu>
2016-04-09 17:10:38 -0400

    Setup for running algorithm from php, bug fixing
    After taking forever to figure out how to get it running, I've got a way
    to run the optimization algorithm from php (calling the python code),
    and to save the output.
    
    We're going to need to install python on our computers though, and
    currently, the way I'm doing it only supports windows computers (running
    python via command line).

--- includes/protocols/optimize.php
+++ includes/protocols/optimize.php
@@ -1,5 +1,27 @@
 <?php
 if(isset($scrubbed["optimize"])) {
     $warnings[] = "This button has not been fully implemented yet.";
+    $pkEventid = $scrubbed["pkEventid"];
+    exec("python \"".$homedir."python/OptCode.py\" $pkEventid", $output);
+    $blCalendar = implode("\n",$output);
+    
+    $q1 = "UPDATE tblevents SET blOptiSuggestion = ? WHERE pkEventid = ?";
+    $q2 = "SELECT nmTitle FROM tblevents WHERE pkEventid = ?";
+    
+    if($stmt = $dbc->prepare($q1)){
+        $stmt->bind_param("si", $blCalendar,$pkEventid);
+        $stmt->execute();
+        $stmt->free_result();
+        $stmt->close();
+    }
+    if($stmt = $dbc->prepare($q2)){
+        $stmt->bind_param("i", $pkEventid);
+        $stmt->execute();
+        $stmt->bind_result($nmTitle);
+        $stmt->fetch();
+        $stmt->free_result();
+        $stmt->close();
+    }
+    $notifications[] = "Mesa has finished calculating optimal meeting times, return to the event page for $nmTitle to view suggestions.";
 }
 ?>
