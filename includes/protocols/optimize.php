45c9b7a754a6af8fe4e0407ded95ef53df1fab43
Jacob <janick@oakland.edu>
2016-04-10 00:50:07 -0400

    Working on passing arguments to algorithm
    Continuing work on optimize.php to send data from the database to the
    algorithm. Decided to move all querying to optimize.php to avoid
    incompatibility issues with various mysql libraries for python.
    
    Also worked on fixing various bugs in OptCode.py to get it into a
    testable state. Still WIP.

--- includes/protocols/optimize.php
+++ includes/protocols/optimize.php
@@ -2,26 +2,57 @@
 if(isset($scrubbed["optimize"])) {
     $warnings[] = "This button has not been fully implemented yet.";
     $pkEventid = $scrubbed["pkEventid"];
-    exec("python \"".$homedir."python/OptCode.py\" $pkEventid", $output);
-    $blCalendar = implode("\n",$output);
     
-    $q1 = "UPDATE tblevents SET blOptiSuggestion = ? WHERE pkEventid = ?";
-    $q2 = "SELECT nmTitle FROM tblevents WHERE pkEventid = ?";
+    $q1 = "SELECT blCalendar FROM tblcalendars cals JOIN tblusers users ON users.pkUserid = cals.fkUserid WHERE cals.fkEventid = ?";
     
+    $calendars = [];
     if($stmt = $dbc->prepare($q1)){
-        $stmt->bind_param("si", $blCalendar,$pkEventid);
-        $stmt->execute();
-        $stmt->free_result();
-        $stmt->close();
-    }
-    if($stmt = $dbc->prepare($q2)){
         $stmt->bind_param("i", $pkEventid);
         $stmt->execute();
-        $stmt->bind_result($nmTitle);
-        $stmt->fetch();
+        $stmt->bind_result($blCalendar);
+        while($stmt->fetch()) {
+            $calendars[] = $blCalendar;
+        }
         $stmt->free_result();
         $stmt->close();
     }
-    $notifications[] = "Mesa has finished calculating optimal meeting times, return to the event page for <b><i>$nmTitle</i></b> to view suggestions.";
+    if(count($calendars) != 0) {
+        
+        $q2 = "SELECT dtStart, dtEnd, txLocation, blSettings, txRRule FROM tblevents WHERE pkEventid = ?";
+        
+        if($stmt = $dbc->prepare($q2)){
+            $stmt->bind_param("i", $pkEventid);
+            $stmt->execute();
+            $stmt->bind_result($dtStart, $dtEnd, $txLocation, $blSettings, $txRRule);
+            $stmt->free_result();
+            $stmt->close();
+        }
+        
+        $calendars = json_encode($calendars);
+        exec("python \"".$homedir."python/OptCode.py\" \"$dtStart\" \"$dtEnd\" \"$txLocation\" $blSettings $txRRule $calendars", $output);
+        $blOptiSuggestion = implode("\n",$output);
+        
+        var_dump($output);
+//        $q3 = "UPDATE tblevents SET blOptiSuggestion = ? WHERE pkEventid = ?";
+//        $q4 = "SELECT nmTitle FROM tblevents WHERE pkEventid = ?";
+//        
+//        if($stmt = $dbc->prepare($q3)){
+//            $stmt->bind_param("si", $blOptiSuggestion,$pkEventid);
+//            $stmt->execute();
+//            $stmt->free_result();
+//            $stmt->close();
+//        }
+//        if($stmt = $dbc->prepare($q4)){
+//            $stmt->bind_param("i", $pkEventid);
+//            $stmt->execute();
+//            $stmt->bind_result($nmTitle);
+//            $stmt->fetch();
+//            $stmt->free_result();
+//            $stmt->close();
+//        }
+//        $notifications[] = "Mesa has finished calculating optimal meeting times, return to the event page for <b><i>$nmTitle</i></b> to view suggestions.";
+    } else {
+        $warnings[] = "None of your attendees have responded to your calendar access request email yet. Please have at least one response before attempting to optimize event settings";
+    }
 }
 ?>
---
+++ python/functions.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="Jacob"
+__date__ ="$Apr 9, 2016 11:56:14 PM$"
+
+def parseRRule():
+    print ("WIP")
