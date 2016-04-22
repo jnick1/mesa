a6d9540d6d657b709dc4046de561661358c477ed
William Thompson <wpthompson@oakland.edu>
2016-04-22 02:13:47 -0400

    Optimized pointList generation. Should take half the time.

--- python/pointListGenerator.py
+++ python/pointListGenerator.py
@@ -5,8 +5,7 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-from datetime import datetime, date, time, timedelta
-import classes
+from datetime import datetime, timedelta
 import math
 
 def construct_point_list(masterMatrix, granularity, baseEvent, blSettings):
@@ -43,40 +42,33 @@ def construct_point_list(masterMatrix, granularity, baseEvent, blSettings):
             return None
         return [0][0,0,0,0]
     else:
-        unconstrained_list = generate_unconstrained_list(masterMatrix)
-        duration_list = generate_duration_list(canModulateDuration, granularity, startingDuration)
         durationIncrement = 0
+        duration_list = generate_duration_list(canModulateDuration, granularity, startingDuration)
         for duration in duration_list:
-            for eventTime in unconstrained_list:
-                if(not canModulateTime):
-                    if(eventTime.time() != startTime.time()):
+            for date in masterMatrix.dates:
+                for time in masterMatrix.times:
+                    eventTime = datetime.combine(date, time)
+                    if(not canModulateTime):
+                        if(eventTime.time() != startTime.time()):
+                            continue
+                    if(not canModulateDate):
+                        if(eventTime.date() != startTime.date()):
+                            continue
+                    attendees = masterMatrix.available_attendees(eventTime, duration);
+                    if(masterMatrix.is_required_attendees_busy(eventTime, duration)):
                         continue
-                if(not canModulateDate):
-                    if(eventTime.date() != startTime.date()):
+                    if(not canModulateAttendees):
+                        if(len(attendees) < len(masterMatrix.attendees)):
+                            continue
+                    if(len(attendees) < minAttendees):
                         continue
-                attendees = masterMatrix.available_attendees(eventTime, duration);
-                if(masterMatrix.is_required_attendees_busy(eventTime, duration)):
-                    continue
-                if(not canModulateAttendees):
-                    if(len(attendees) < len(masterMatrix.attendees)):
-                        continue
-                if(len(attendees) < minAttendees):
-                    continue
-                diffDates = (datetime.combine(startTime.date(),time(0,0,0)) - datetime.combine(eventTime.date(), time(0,0,0))).days
-                diffTimes = (datetime.combine(date(1,1,1),startTime.time()) - datetime.combine(date(1,1,1),eventTime.time())).total_seconds()
-                diffTimes = math.ceil(diffTimes/(60*granularity)) #Only thing I don't know if works
-                datetimePoint = [-1 * diffTimes,-1 * diffDates,durationIncrement,(len(masterMatrix.attendees) - len(attendees))]
-                finalPointList.append(datetimePoint)
+                    diffDates = (datetime.combine(startTime.date(),time(0,0,0)) - datetime.combine(eventTime.date(), time(0,0,0))).days
+                    diffTimes = (datetime.combine(date(1,1,1),startTime.time()) - datetime.combine(date(1,1,1),eventTime.time())).total_seconds()
+                    diffTimes = math.ceil(diffTimes/(60*granularity))
+                    datetimePoint = [-1 * diffTimes,-1 * diffDates,durationIncrement,(len(masterMatrix.attendees) - len(attendees))]
+                    finalPointList.append(datetimePoint)
             durationIncrement += 1
     return finalPointList
-        
-def generate_unconstrained_list(masterMatrix):
-    unconstrained_list = []
-    for date in masterMatrix.dates:
-        for time in masterMatrix.times:
-            unconstrained_list.append(datetime.combine(date, time))
-    
-    return unconstrained_list;
 
 def generate_duration_list(canModulateDuration, granularity, startingDuration):
     duration_list = [startingDuration]
